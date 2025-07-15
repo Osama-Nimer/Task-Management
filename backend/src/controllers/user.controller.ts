7873183d3b8aec84bd8cfff2b13299eb90be121e
Osama-Nimer <osamafares23@yahoo.com>
2025-07-15 15:36:39 +0300

    user Done

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request , Response } from "express";
 import { db } from "../configs/db.config";
 import { users } from "../db/schema";
-
+import { eq } from 'drizzle-orm';
 export const getAllUsers = async  (req : Request , res : Response)=>{
     try {
         const allUsers = await db
@@ -19,6 +19,64 @@ export const getAllUsers = async  (req : Request , res : Response)=>{
         }
         res.status(200).json({Message : "Data Fetched Successfully :) " , allUsers});
         return;
+    } catch (error : any) {
+        res.status(500).json({Message :error.message });
+        return;
+    }
+}
+
+
+export const getUserbyeamil = async (req : Request , res : Response)=>{
+    const { email } = req.body;
+    if(!email){
+        res.status(404).json({message : "no data parsing"});
+        return;
+    }
+    try {
+        const user = await db.select(
+        {
+            id : users.id,
+            first_name : users.firstName, 
+            last_name : users.lastName ,
+            email : users.email,
+            Role : users.role 
+        }
+    ).from(users)
+    .where(eq(users.email ,email)).limit(1);
+    if(!user){
+        res.status(404).json({Message : "Invalid data , there is no user Found !!"});
+        return;
+    }
+    res.status(200).json({message : "User Found Successfully :)" , user});
+    return;
+    } catch (error : any) {
+        res.status(500).json({Message :error.message });
+        return;
+    }
+}
+export const getUserbyId = async (req : Request , res : Response)=>{
+    const { id } = req.body;
+    if(!id){
+        res.status(404).json({message : "no data parsing"});
+        return;
+    }
+    try {
+        const user = await db.select(
+        {
+            id : users.id,
+            first_name : users.firstName, 
+            last_name : users.lastName ,
+            email : users.email,
+            Role : users.role 
+        }
+    ).from(users)
+    .where(eq(users.id ,id)).limit(1);
+    if(!user){
+        res.status(404).json({Message : "Invalid data , there is no user Found !!"});
+        return;
+    }
+    res.status(200).json({message : "User Found Successfully :)" , user});
+    return;
     } catch (error : any) {
         res.status(500).json({Message :error.message });
         return;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { getAllUsers } from "../controllers/user.controller";
+import { getAllUsers , getUserbyeamil} from "../controllers/user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { isAuthorizedRole } from "../middlewares/role.middleware";
 const router = express.Router();
@@ -8,6 +8,12 @@ router.get("/getAllUsers",
         authMiddleware ,
         isAuthorizedRole("Admin") ,
         getAllUsers
+    );
+
+router.get("/getUserByEmail",
+        authMiddleware ,
+        isAuthorizedRole("Admin"),
+        getUserbyeamil
     );
 
 export default router;
